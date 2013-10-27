50ad8b743d5b4dc1729964f52404d9b5f6c3093d
GlenDC <decauwsemaecker.glen@gmail.com>
2013-10-27 21:15:17 +0100

    Game is ignored

--- jni/Application.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-override APP_MIN_PLATFORM_LEVEL=99
-NDK_TOOLCHAIN_VERSION=4.8
-APP_STL:=gnustl_static
-APP_CFLAGS += -Wno-error=format-security
-APP_GNUSTL_FORCE_CPP_FEATURES := exceptions rtti
