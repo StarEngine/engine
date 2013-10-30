6f496f101a0b05a2da858b6515d26921b8cad10b
NuclearCookie <pietervantorre@gmail.com>
2013-10-30 02:52:27 +0200

    Included the libPng in our engine instead of in the ndk-dir. however, the project doesn't use this location yet. Please help. Also removed some unnecessairy things from the Android.mk file

--- StarEngine/jni/Android.mk
+++ StarEngine/jni/Android.mk
@@ -6,18 +6,18 @@ LS_CPP=$(subst $(1)/,,$(wildcard $(1)/*.cpp))
 LS_CPP+= $(subst $(1)/,,$(wildcard $(1)/**/*.cpp))
 LS_CPP+= $(subst $(1)/,,$(wildcard $(1)/**/**/*.cpp))
 LS_CPP+= $(subst $(1)/,,$(wildcard $(1)/**/**/**/*.cpp))
+
 LOCAL_MODULE    := starengine
 LOCAL_CFLAGS 	:= -D FT_FREETYPE_H="freetype/freetype.h"
 LOCAL_CPPFLAGS  := -std=gnu++11 -D STAR2D=1 -D LOGGER_MIN_LEVEL=1 -I libs\freetype\jni\include
 LOCAL_SRC_FILES := $(call LS_CPP,$(LOCAL_PATH))
-APP_PLATFORM := android-14
 
 LOCAL_STATIC_LIBRARIES := android_native_app_glue png freetype
 
 include $(BUILD_STATIC_LIBRARY)
 
 $(call import-module,android/native_app_glue)
-$(call import-module,libpng)
 
 $(call import-add-path,libs)
 $(call import-module,freetype)
+$(call import-module,libpng)
