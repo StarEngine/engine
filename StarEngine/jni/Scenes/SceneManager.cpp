e190cade4cd097df5bc196b8a7b08320051b32f8
GlenDC <decauwsemaecker.glen@gmail.com>
2013-12-05 23:54:29 +0100

    Bugfix related to an old issue for objects, now also applied to scenes.

--- StarEngine/jni/Scenes/SceneManager.cpp
+++ StarEngine/jni/Scenes/SceneManager.cpp
@@ -159,7 +159,6 @@ namespace star
 		if(it != m_SceneList.end())
 		{
 			m_GarbageList.push_back(it->second);
-			m_SceneList.erase(it);
 			return true;
 		}
 		return false;
@@ -192,6 +191,8 @@ namespace star
 
 		for(auto scene : m_GarbageList)
 		{
+			auto it = m_SceneList.find(scene->GetName());
+			m_SceneList.erase(it);
 			SafeDelete(scene);
 		}
 		m_GarbageList.clear();
